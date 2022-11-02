dcc00351bd5e890de5f58513fbfb52b606b925ae
Jeremy Schmersal <jschmersal1@cscc.edu>
2022-11-01 21:17:16 -0400

    update to accommodate Jenkins changes

--- docker/init.groovy.d/startup.groovy
+++ docker/init.groovy.d/startup.groovy
@@ -8,10 +8,6 @@ import jenkins.model.GlobalConfiguration
   script creates and runs the seed job (which then creates the other jobs for this jenkins instance
 */
 
-// Disable global level warning about security
-hudson.model.AdministrativeMonitor.all()
-        .grep{ it instanceof jenkins.security.s2m.MasterKillSwitchWarning }
-        .each{ it.disable(true) }
 // Disable warning that new version of Jenkins available
 hudson.model.AdministrativeMonitor.all()
         .grep{ it instanceof hudson.model.UpdateCenter$CoreUpdateMonitor }
